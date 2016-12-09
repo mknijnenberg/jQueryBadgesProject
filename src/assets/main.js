f6f5b57f338c0a94e0aeec32bddad678844a4fa3
Maarten Knijnenberg <m.knijnenberg@gmail.com>
2016-12-09 11:55:29 +0100

    first commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,42 @@
-$(function($) {
+$(function() {
 
-$ $.ajax({
+$.ajax({
     url: 'https://www.codeschool.com/users/mknijnenberg.json',
     dataType: 'jsonp',
     success: function(response) {
-      // handle response
+      addCourses(response.courses.completed);
     }
   });
 
-})(jQuery);
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      console.log(course);
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course',
+      }).appendTo($course);
+
+    })
+
+  }
+
+});
